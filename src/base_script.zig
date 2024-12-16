cd85b0289977cbfc6772a4335e926130679ec2c0
Tai Kedzierski <dch.tai@gmail.com>
2024-12-16 11:38:24 +0000

    Make stub not fail main

--- src/base_script.zig
+++ src/base_script.zig
@@ -1,18 +1,21 @@
-const BASE_SCRIPT =
-    //const std = @import("std");
-    //
-    //pub fn build(b:*std.Build) void {
-    //    const exe = b.addExecutable(.{
-    //        .name = "%NAME%",
-    //        .root_source_file = b.path("%MAINPATH%"),
-    //        .target = b.standardTargetOptions(.{}),
-    //        .optimize = b.standardOptimizeOption(.{}),
-    //    });
-    //
-    //    b.installArtifact(exe);
-    //}
+const BASE_SCRIPT:[]const u8 =
+    \\const std = @import("std");
+    \\
+    \\pub fn build(b:*std.Build) void {
+    \\    const exe = b.addExecutable(.{
+    \\        .name = "%NAME%",
+    \\        .root_source_file = b.path("%MAINPATH%"),
+    \\        .target = b.standardTargetOptions(.{}),
+    \\        .optimize = b.standardOptimizeOption(.{}),
+    \\    });
+    \\
+    \\    b.installArtifact(exe);
+    \\}
 ;
 
 pub fn getScript(buf:*[]u8, name:[]const u8, path:[]const u8) void {
 	// FIXME - sub name and path into the base script, and return the result
+    _ = buf;
+    _ = name;
+    _ = path;
 }
